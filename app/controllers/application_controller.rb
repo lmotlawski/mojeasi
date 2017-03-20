ca711a8a1f1cb172f681ace5715e6f068acd6bfb
AdlerPoland <resepoland@o2.pl>
2017-03-20 16:19:23 +0100

    photo display

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!, :except => [:show, :index,:info]
+  helper FilepickerRails::Engine.helpers
 end
