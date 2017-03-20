7e58be292600d302dea2603bec8292e74640f2cf
AdlerPoland <resepoland@o2.pl>
2017-03-20 16:55:29 +0100

    cloudinary integration

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,5 @@ module Photogallery
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-    config.filepicker_rails.api_key = "AHMuhXQQSSziePZ1qgdxEz"
   end
 end
