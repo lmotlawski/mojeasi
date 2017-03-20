b080c6c6c5c23c335a7a2a732cdd1e4306a574f7
AdlerPoland <resepoland@o2.pl>
2017-03-20 17:14:10 +0100

    photo author

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -2,5 +2,6 @@ class Photo < ApplicationRecord
     mount_uploader :file, PictureUploader
     has_many :comments
     belongs_to :user
+    delegate :name, :to => :user, :prefix => true
     validates :title, length: { minimum: 5 }
 end
